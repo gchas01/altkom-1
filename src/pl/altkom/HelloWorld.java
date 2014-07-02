580e91ee7bf0c6968f8c079129334401c3443dec
gchas <gchas@HOUSE-COMP>
2014-07-02 21:55:01 +0200

    alt-03

--- src/pl/altkom/HelloWorld.java
+++ src/pl/altkom/HelloWorld.java
@@ -2,7 +2,7 @@ package pl.altkom;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main2(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hjhjhjhj");
 	}
