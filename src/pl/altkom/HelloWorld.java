c34d0881fa53c9ec1a771b3b8b5948d862e7aa87
gchas <gchas@soft-project.pl>
2014-07-02 22:02:59 +0200

    alt-04

--- src/pl/altkom/HelloWorld.java
+++ src/pl/altkom/HelloWorld.java
@@ -2,7 +2,7 @@ package pl.altkom;
 
 public class HelloWorld {
 
-	public static void main1(String[] args) {
+	public static void main3(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hjhjhjhj");
 	}
